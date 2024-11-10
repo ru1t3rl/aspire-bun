d92f41c36f05fe52243f69ec754c43828f4652ce
ru1t3rl <lucaruiters@hotmail.nl>
2024-11-10 13:37:41 +0100

    Initial commit

---
+++ Aspire.Hosting.Bun/NodeAppHostingExtension.cs
@@ -0,0 +1,31 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using Microsoft.Extensions.Hosting;
+
+namespace Aspire.Hosting.Bun;
+
+public static class NodeAppHostingExtension
+{
+    public static IResourceBuilder<NodeAppResource> AddBunApp(this IDistributedApplicationBuilder builder, string name, string workingDirectory, string scriptName = "start", bool useBunx = true, string[]? args = null)
+    {
+
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(name);
+        ArgumentNullException.ThrowIfNull(workingDirectory);
+        ArgumentNullException.ThrowIfNull(scriptName);
+
+        string[] allArgs = args is { Length: > 0 }
+            ? ["run", scriptName, "--", .. args]
+            : ["run", scriptName];
+
+        workingDirectory = PathNormalizer.NormalizePathForCurrentPlatform(Path.Combine(builder.AppHostDirectory, workingDirectory));
+        var resource = new NodeAppResource(name, useBunx? "bunx --hot --bun" : "bun --hot --bun", workingDirectory);
+
+        return builder.AddResource(resource)
+            .WithNodeDefaults()
+            .WithArgs(allArgs);
+    }
+    
+    private static IResourceBuilder<NodeAppResource> WithNodeDefaults(this IResourceBuilder<NodeAppResource> builder) =>
+        builder.WithOtlpExporter()
+            .WithEnvironment("NODE_ENV", builder.ApplicationBuilder.Environment.IsDevelopment() ? "development" : "production");
+}
---
+++ Aspire.Hosting.Bun/PathNormalizer.cs
@@ -0,0 +1,17 @@
+﻿namespace Aspire.Hosting.Bun;
+
+internal static class PathNormalizer
+{
+    public static string NormalizePathForCurrentPlatform(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            return path;
+        }
+
+        // Fix slashes
+        path = path.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
+
+        return Path.GetFullPath(path);
+    }
+}
