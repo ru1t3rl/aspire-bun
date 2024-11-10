03bcf9421c568a970d33f13ac6508bfe69338074
ru1t3rl <lucaruiters@hotmail.nl>
2024-11-10 20:52:44 +0100

    Change bun and bunx implementation

--- Aspire.Hosting.Bun/NodeAppHostingExtension.cs
+++ Aspire.Hosting.Bun/NodeAppHostingExtension.cs
@@ -5,27 +5,49 @@ namespace Aspire.Hosting.Bun;
 
 public static class NodeAppHostingExtension
 {
-    public static IResourceBuilder<NodeAppResource> AddBunApp(this IDistributedApplicationBuilder builder, string name, string workingDirectory, string scriptName = "start", bool useBunx = true, string[]? args = null)
+    public static IResourceBuilder<NodeAppResource> AddBunApp(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        string workingDirectory,
+        string scriptName = "start",
+        bool useBunx = true,
+        string[]? args = null)
     {
-
         ArgumentNullException.ThrowIfNull(builder);
         ArgumentNullException.ThrowIfNull(name);
         ArgumentNullException.ThrowIfNull(workingDirectory);
         ArgumentNullException.ThrowIfNull(scriptName);
 
-        string[] allArgs = args is { Length: > 0 }
-            ? ["run", scriptName, "--", .. args]
-            : ["run", scriptName];
+        string[] allArgs;
 
-        workingDirectory = PathNormalizer.NormalizePathForCurrentPlatform(Path.Combine(builder.AppHostDirectory, workingDirectory));
-        var resource = new NodeAppResource(name, useBunx? "bunx --hot --bun bun" : "bun --hot", workingDirectory);
+        if (useBunx)
+        {
+            allArgs = args is { Length: > 0 }
+                ? ["--hot --bun bun run", scriptName, "--", .. args]
+                : ["--hot --bun bun run", scriptName];
+        }
+        else
+        {
+            allArgs = args is { Length: > 0 }
+                ? ["--hot run", scriptName, "--", .. args]
+                : ["--hot run", scriptName];
+        }
+
+        workingDirectory =
+            PathNormalizer.NormalizePathForCurrentPlatform(Path.Combine(builder.AppHostDirectory, workingDirectory));
+
+        var resource = new NodeAppResource(name, useBunx
+            ? "bunx"
+            : "bun", workingDirectory);
 
         return builder.AddResource(resource)
             .WithNodeDefaults()
             .WithArgs(allArgs);
     }
-    
+
     private static IResourceBuilder<NodeAppResource> WithNodeDefaults(this IResourceBuilder<NodeAppResource> builder) =>
         builder.WithOtlpExporter()
-            .WithEnvironment("NODE_ENV", builder.ApplicationBuilder.Environment.IsDevelopment() ? "development" : "production");
+            .WithEnvironment("NODE_ENV", builder.ApplicationBuilder.Environment.IsDevelopment()
+                ? "development"
+                : "production");
 }
